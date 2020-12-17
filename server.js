7afbbfc20125b47c2262a2fc8406d8cb99c7d2fc
Levente Balogh <balogh.levente.hu@gmail.com>
2020-12-17 08:49:49 +0100

    fix: change that funky comment!

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const logger = createLogger({
   ]
 })
 
-// Send logs every 1 second
+// Send random logs repeatedly
 setInterval(() => {
   const level = getRandomArrayElement(['debug', 'info', 'warn', 'error']);
   const labels = getRandomArrayElement([{ env: 'dev' }, { env: 'prod' }]);
